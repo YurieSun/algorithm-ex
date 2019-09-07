f339f8dc923424b7ad4bc0d3d7048eae07a728a2
YurieSun <757075280@qq.com>
2019-09-07 17:12:39 +0800

    submit ex_1.1.3

---
+++ ex_1.1.3.java
@@ -0,0 +1,21 @@
+/**
+ * Get three integers from input, and judge if all of them are equal.
+ * Todo: print tips when not all inputs are integers. 
+ */
+import java.util.Scanner;
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class Test{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[3];
+        System.out.println("input three numbers");
+        for (int i = 0; i < 3; i++)
+            arr[i] = in.nextInt();
+        if (arr[0] == arr[1] && arr[1] == arr[2])
+            System.out.println("equal");
+        else
+            System.out.println("not equal");
+        in.close();       
+    }
+}
